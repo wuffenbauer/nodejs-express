cc00cb27bbe937cbf45ad277013def98a4eaf275
wuffenbauer <wuffenbauer@gmail.com>
2024-06-14 21:19:05 +0700

    install ejs, setting ejs, pisah view

--- app.js
+++ app.js
@@ -1,1 +1,18 @@
-console.log('hello world')
+const express = require('express')
+const app     = express()
+const port    = 3000
+
+app.set('view engine', 'ejs')   // setting penggunaan template engine untuk express
+app.set('views', './view-ejs')  // setting penggunaan folder untuk mentimpan file .ejs
+
+app.get('/', (req, res) => {
+    res.render('index')
+})
+
+app.get('/profil', (req, res) => {
+    res.render('profil')
+})
+
+app.listen(port, () => {
+    console.log(`App sudah siap, buka http://localhost:${port}`)
+})
